472e6f2d4abfe8cec2ba88aafe38bc2e43b767d6
swana422 <swana422@yahoo.com>
2018-03-19 14:29:13 -0400

    Chap1 DO loop

---
+++ Desktop/jspractice.js
@@ -0,0 +1,9 @@
+var numOfCustomers = 5
+
+do {
+		console.log( "How may I help you?" );
+		
+		numOfCustomers = numOfCustomers - 1
+} while (numOfCustomers > 0;
+
+console.log( "Time for a beer!" );
